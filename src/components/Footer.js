7146b2d13ee75769a82c5b910741b4dcb34b5252
kris <krisekteles@gmail.com>
2025-08-27 22:55:08 +0300

    list of products

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,14 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-        footer section
+       <div className='product_footer'>
+<h4>our product</h4>
+<ul>
+  <li>sculptures</li>
+  <li>Drawings</li>
+  <li>paintings</li>
+</ul>
+       </div>
       
     </div>
   )
