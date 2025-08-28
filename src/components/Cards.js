be3757049f4597125656d570b259bf4856d07064
kris <krisekteles@gmail.com>
2025-08-28 16:29:53 +0300

    cards component

---
+++ src/components/Cards.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='card'>
+      <img src='' alt/>
+      <h3>first pic</h3>
+      <p>this is the description</p>
+      <p>this is price </p>
+      <button>add</button>
+      
+    </div>
+  )
+}
+
+export default Cards
