da223d1a4e8062630c424ac587756509b869dc06
kris <krisekteles@gmail.com>
2025-08-28 18:44:25 +0300

    change button appearance

--- src/components/Authorise.js
+++ src/components/Authorise.js
@@ -8,15 +8,16 @@ const Authorise = () => {
       <div className="login">
         <form method="Post">
           <h3>Log In</h3>
+          <input type='input' placeholder='name'/>
           <label>Email</label>
           <input type="email" placeholder="example name@gmail.com" required />
           <label>password</label>
           <input type="password" placeholder="Enter password" required />
-          <button type="submit"> Log in</button>
-          <p>
-            Dont have an account?
-            <Link to="signup">signUp</Link>
-          </p>
+          <div className='submit-class'>
+            <div className='submit'>login</div>
+            <div className='submit'>signUp</div>
+          </div>
+          
         </form>
       </div>
     
