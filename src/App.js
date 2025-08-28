d9cb6d98cb742adc2355646857e694626edf1916
kris <krisekteles@gmail.com>
2025-08-28 18:04:11 +0300

    home page render

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Link, Route, Routes } from "react-router-dom";
+import Home from './components/Home';
 import About from './components/About';
 import Authorise from './components/Authorise';
 import Footer from './components/Footer';
@@ -12,29 +13,31 @@ function App() {
   return (
     <div className="App">
       <nav>
+        <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/logIn/signUp">LogIn/SignUp</Link>
       </nav>
 
       <Routes>
+        <Route path="/#" element={<Home />} />
         <Route path="/logIn/signUp" element={<Authorise />} />
         <Route path="/About" element={<About />} />
       </Routes>
-<div className='painting'>
-      <Cards
-        // img={nature}
-        name="painting"
-        description="this is the nature painting"
-        price="100 k"
-        button="order"
-      />
-      <Cards
-        // img={nature}
-        name="nature"
-        description="this is the nature painting"
-        price="50 k"
-        button="order"
-      />
+      <div className="painting">
+        <Cards
+          // img={nature}
+          name="painting"
+          description="this is the nature painting"
+          price="100 k"
+          button="order"
+        />
+        <Cards
+          // img={nature}
+          name="nature"
+          description="this is the nature painting"
+          price="50 k"
+          button="order"
+        />
       </div>
       {/* footer section component*/}
       <Footer />
