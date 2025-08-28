c72c335b8894a35bd80afab010e6fce31abb4411
kris <krisekteles@gmail.com>
2025-08-28 11:56:48 +0300

    login form

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { Link, Route, Routes } from "react-router-dom";
 import About from './components/About';
+import Authorise from './components/Authorise';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/about">About</Link>
+        <Link to="/logIn/signUp">LogIn/SignUp</Link>
       </nav>
+      
       <Routes>
-        <Route path="/about" element={<About />}/>
+        <Route path="/logIn/signUp" element={<Authorise />}/>
+        
       </Routes>
       {/* footer section component*/}
       <Footer />  
