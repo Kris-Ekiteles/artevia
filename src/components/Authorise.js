714d0a0720d1211bba43772518ae8f2237d33f9f
kris <krisekteles@gmail.com>
2025-08-28 12:13:59 +0300

    div to wrap form in / form content

--- src/components/Authorise.js
+++ src/components/Authorise.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./Authorise.css"
 
 const Authorise = () => {
   return (
-    <div className="login">
-      <form method="Post">
-        <h3>Log In</h3>
-        <label>Email</label>
-        <input type="email" placeholder="example name@gmail.com" required />
-        <label>password</label>
-        <input type="password" placeholder="Enter password" required />
-        <button type="submit"> Log in</button>
-        <p>
+    
+      <div className="login">
+        <form method="Post">
+          <h3>Log In</h3>
+          <label>Email</label>
+          <input type="email" placeholder="example name@gmail.com" required />
+          <label>password</label>
+          <input type="password" placeholder="Enter password" required />
+          <button type="submit"> Log in</button>
+          <p>
             Dont have an account?
-          <Link to="signup">signUp</Link>
-        </p>
-      </form>
-    </div>
+            <Link to="signup">signUp</Link>
+          </p>
+        </form>
+      </div>
+    
   );
 }
 
