1fa1879f7642191dcb33ed0062a1b6889703ceea
kris <krisekteles@gmail.com>
2025-08-28 17:06:20 +0300

    image rendered

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import "./Cards.css"
+import nature from "../Assets/abstract-nature-painted.jpg";
 
 const Cards = () => {
   return (
-    <div className='card'>
-      <img src='' alt/>
+    <div className="card">
+      <img src={nature}/>
       <h3>first pic</h3>
       <p>this is the description</p>
       <p>this is price </p>
       <button>add</button>
-      
     </div>
-  )
+  );
 }
 
 export default Cards
