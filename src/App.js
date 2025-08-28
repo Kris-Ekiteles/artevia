24d3e35a5723476bea86b46f17d7f16a5fe790e0
kris <krisekteles@gmail.com>
2025-08-28 17:27:56 +0300

    cards from props

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About';
 import Authorise from './components/Authorise';
 import Footer from './components/Footer';
 import Cards from './components/Cards';
+import nature from "./Assets/abstract-nature-painted.jpg";
 
 function App() {
   return (
@@ -16,9 +17,23 @@ function App() {
 
       <Routes>
         <Route path="/logIn/signUp" element={<Authorise />} />
-        <Route path="/About" element={<About/>} />
+        <Route path="/About" element={<About />} />
       </Routes>
-      <Cards />     
+      
+      <Cards
+        img={nature}
+        name="nature"
+        description="this is the nature painting"
+        price="50 k"
+        button="order"
+      />
+      <Cards
+        img={nature}
+        name="nature"
+        description="this is the nature painting"
+        price="50 k"
+        button="order"
+      />
       {/* footer section component*/}
       <Footer />
     </div>
