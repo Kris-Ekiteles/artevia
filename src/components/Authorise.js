a23882482c8adf154ad4966440acdcdc5469c2f4
kris <krisekteles@gmail.com>
2025-08-28 20:13:27 +0300

    set action and usestate

--- src/components/Authorise.js
+++ src/components/Authorise.js
@@ -1,26 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./Authorise.css"
 
 const Authorise = () => {
+  const [action, setAction] = useState("sign Up")
   return (
-    
-      <div className="login">
-        <form method="Post">
-          <h3>Log In</h3>
-          <input type='input' placeholder='name'/>
-          <label>Email</label>
-          <input type="email" placeholder="example name@gmail.com" required />
-          <label>password</label>
-          <input type="password" placeholder="Enter password" required />
-          <div className='submit-class'>
-            <div className='submit'>login</div>
-            <div className='submit'>signUp</div>
+    <div className="login">
+      <form method="Post">
+        <h3>{action}</h3>
+        {action === "login" ? (
+          <div></div>
+        ) : (
+          <div>
+            <input type="input" placeholder="name" />
           </div>
-          
-        </form>
-      </div>
-    
+        )}
+
+        <label>Email</label>
+        <input type="email" placeholder="example name@gmail.com" required />
+        <label>password</label>
+        <input type="password" placeholder="Enter password" required />
+        {action === "login" ? (
+          <div></div>
+        ) : (
+          <div className="Forgot">
+            <p>Forgot password</p>
+          </div>
+        )}
+
+        <div className="submit-class">
+          <div
+            className={action === "login" ? "submit gray" : "submit"}
+            onClick={() => {
+              setAction("login");
+            }}
+          >
+            login
+          </div>
+          <div
+            className={action === "sign Up" ? "submit gray" : "submit"}
+            onClick={() => {
+              setAction("sign Up");
+            }}
+          >
+            sign Up
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
 
